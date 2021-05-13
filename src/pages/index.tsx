4aa2f601f1ef61f3e977ad1eb52a9876457cabfe
NicholasNeto <nicholasneto@outlook.com>
2021-05-12 22:23:22 -0300

    page login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,50 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Flex, Input, Button, FormLabel, Stack, FormControl } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <div >
-      <h1>Hello Word Dashgo</h1>
-    </div>
+    <Flex width='100vw' height='100vh' alignItems='center' justifyContent='center'>
+      <Flex
+        as='form'
+        width='100%'
+        maxWidth={360}
+        backgroundColor='gray.800'
+        p='8'
+        borderRadius={8}
+        flexDirection='column'>
+        <Stack spacing='4'>
+          <FormControl>
+            <FormLabel id='email'>E-mail</FormLabel>
+            <Input
+              id='email'
+              name='email'
+              type='email'
+              size='lg'
+              backgroundColor='gray.900'
+              focusBorderColor='pink.500'
+              variant='filled'
+              _hover={{
+                backgroundColor: 'gray.900'
+              }}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel id='password' >Senha</FormLabel>
+            <Input
+              name='password'
+              type='password'
+              id='password'
+              size='lg'
+              backgroundColor='gray.900'
+              focusBorderColor='pink.500'
+              variant='filled'
+              _hover={{
+                backgroundColor: 'gray.900'
+              }}
+            />
+          </FormControl>
+          <Button type='submit' marginTop='6' colorScheme='pink' size='lg'>Entrar</Button>
+        </Stack>
+      </Flex>
+    </Flex>
   )
 }
