98e5d766c31c63f7ffce03e15b515eb4454ac319
NicholasNeto <nicholasneto@outlook.com>
2021-05-12 22:50:56 -0300

    change name of page SignIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Flex,  Button, Stack } from '@chakra-ui/react'
 import { Input } from '../components/Form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
     <Flex width='100vw' height='100vh' alignItems='center' justifyContent='center'>
       <Flex
