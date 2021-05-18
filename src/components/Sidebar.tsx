0886a852ccc9e9d29029fde4c424cd9ac5b5d132
NicholasNeto <nicholasneto@outlook.com>
2021-05-17 21:43:25 -0300

    create navagation in sidebar firts geral - dasboard

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,21 @@
-export function Sidebar(){
+
+import { Box, Stack, Text, Icon, Link } from "@chakra-ui/react";
+import { RiDashboardLine } from "react-icons/ri";
+
+export function Sidebar() {
     return (
-        <div>
-            Sidebar
-        </div>
+        <Box as='aside' w='64' mr='8'>
+            <Stack spacing='12' align='flex-start'>
+                <Box>
+                    <Text fontWeight='bold' color='gray.400' fontSize='small'>GERAL</Text>
+                    <Stack spacing='4' mt='8' align='center'>
+                        <Link display='flex' align='center'>
+                            <Icon as={RiDashboardLine} fontSize='20' />
+                            <Text ml='4' fontWeight='medium' >Dashboard</Text>
+                        </Link>
+                    </Stack>
+                </Box>
+            </Stack>
+        </Box>
     )
 }
