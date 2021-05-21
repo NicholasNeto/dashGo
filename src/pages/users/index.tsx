53fd194198136b20881e4dc2b17e2ebd628fbf75
NicholasNeto <nicholasneto@outlook.com>
2021-05-20 22:12:24 -0300

    add more lines in table

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -52,6 +52,36 @@ export default function UserList() {
                                     <Button as='a' size='sm' fontSize='sm' colorScheme='purple' leftIcon={<Icon as={RiPencilLine} fontSize='16'  />}> Editar</Button>
                                 </Td>
                             </Tr>
+                            <Tr>
+                                <Td px='6'>
+                                    <Checkbox colorScheme='pink' />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight='bold'> Nicholas Neto</Text>
+                                        <Text fontSize='sm' color='gray.300'> nicholasneto@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td> 04 de Abril, 2011 </Td>
+                                <Td>
+                                    <Button as='a' size='sm' fontSize='sm' colorScheme='purple' leftIcon={<Icon as={RiPencilLine} fontSize='16'  />}> Editar</Button>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td px='6'>
+                                    <Checkbox colorScheme='pink' />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight='bold'> Nicholas Neto</Text>
+                                        <Text fontSize='sm' color='gray.300'> nicholasneto@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td> 04 de Abril, 2011 </Td>
+                                <Td>
+                                    <Button as='a' size='sm' fontSize='sm' colorScheme='purple' leftIcon={<Icon as={RiPencilLine} fontSize='16'  />}> Editar</Button>
+                                </Td>
+                            </Tr>
                         </Tbody>
                     </Table>
                 </Box>
