7de1bb13b922cecb7164b24ddcf5490ee228985b
NicholasNeto <nicholasneto@outlook.com>
2021-06-01 20:31:07 -0300

    used isWideVersion for render same informations in Profile

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,22 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react"
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align='center'>
-            <Box mr='4' textAlign='right'>
-                <Text>Nicholas Neto</Text>
-                <Text
-                    color='gray.300'
-                    fontSize='small'>
-                    nicholasneto@gmail.com
+            {showProfileData && (
+                <Box mr='4' textAlign='right'>
+                    <Text>Nicholas Neto</Text>
+                    <Text
+                        color='gray.300'
+                        fontSize='small'>
+                        nicholasneto@gmail.com
                         </Text>
-            </Box>
-
+                </Box>
+            )}
             <Avatar size='md' name='Nicholas Neto' src='https://github.com/nicholasneto.png' />
         </Flex>
     )
