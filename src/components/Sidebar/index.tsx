612490c645fef40cd587593aca0ae00e6d925f38
NicholasNeto <nicholasneto@outlook.com>
2021-06-04 16:27:57 -0300

    used sidebarDrawerContext

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,32 @@
-
-import { Box } from "@chakra-ui/react";
+import { Box, Drawer, DrawerOverlay, DrawerContent, DrawerHeader, DrawerBody, DrawerCloseButton, useBreakpointValue } from "@chakra-ui/react";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContenxt";
 import { SidebarNav } from "./SidebarNav";
 
 export function Sidebar() {
+    const {isOpen, onClose } = useSidebarDrawer()
+
+    const isDrawerSidebar = useBreakpointValue({
+        base: true,
+        lg: false,
+    })
+
+    if (isDrawerSidebar) {
+        return (
+            <Drawer isOpen={isOpen} placement='left' onClose={onClose}>
+                <DrawerOverlay>
+                    <DrawerContent backgroundColor='gray.800' padding="4">
+                        <DrawerCloseButton mt='6' />
+                        <DrawerHeader>Navegação</DrawerHeader>
+                        <DrawerBody>
+                            <SidebarNav />
+                        </DrawerBody>
+                    </DrawerContent>
+                </DrawerOverlay>
+            </Drawer>
+        )
+    }
+
+
     return (
         <Box as='aside' w='64' mr='8'>
             <SidebarNav />
