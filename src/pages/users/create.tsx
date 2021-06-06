b5a49a581d623d07ef9ebf7c1690a2d29b0a5df7
NicholasNeto <nicholasneto@outlook.com>
2021-06-06 20:48:18 -0300

    make component create user responsivi

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -11,16 +11,16 @@ export default function CreateUser() {
             <Header />
             <Flex width="100%" my='6' maxWidth={1480} mx='auto' px='6'>
                 <Sidebar />
-                <Box flex='1' borderRadius={8} background='gray.800' padding='8'>
+                <Box flex='1' borderRadius={8} background='gray.800' padding={['6', '8']}>
                     <CustomizedTitle>Criar usuário</CustomizedTitle>
                     <Divider my='6' borderColor='gary.700' />
                     <VStack spacing='8'>
-                        <SimpleGrid minChildWidth='240px' spacing='8' width='100%'>
+                        <SimpleGrid minChildWidth='240px' spacing={['6', '8']} width='100%'>
                             <Input name='name' label='Nome completo' />
                             <Input name='email' type='email' label='E-mail' />
                         </SimpleGrid>
 
-                         <SimpleGrid minChildWidth='240px' spacing='8' width='100%'>
+                         <SimpleGrid minChildWidth='240px' spacing={['6', '8']} width='100%'>
                             <Input name='password'  type='password' label='Senha' />
                             <Input name='password_confirmation' type='password' label='Confirmação da senha'/>
                         </SimpleGrid>
