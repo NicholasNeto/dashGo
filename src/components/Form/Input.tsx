1b3fcc6f0041e89e66ff136be5e2ba159a8b346d
NicholasNeto <nicholasneto@outlook.com>
2021-06-10 08:02:08 -0300

    used forwardRef in Input and created InputBase

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,20 +1,19 @@
+import { forwardRef, ForwardRefRenderFunction } from 'react'
 import { FormControl, FormLabel, Input as ChakraInput, InputProps as chakraInputProps } from '@chakra-ui/react'
 
 
-interface InputPropsType extends chakraInputProps {
+interface InputProps extends chakraInputProps {
     name: string;
     label?: string
 }
 
-
-export function Input({ name, label, ...rest }: InputPropsType) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, ...rest }, ref) =>  {
     return (
         <FormControl>
             {!!label && <FormLabel htmlFor={name} >{label}</FormLabel>}
             <ChakraInput
                 name={name}
                 id={name}
-                {...rest}
                 size='lg'
                 backgroundColor='gray.900'
                 focusBorderColor='pink.500'
@@ -22,7 +21,11 @@ export function Input({ name, label, ...rest }: InputPropsType) {
                 _hover={{
                     backgroundColor: 'gray.900'
                 }}
+                {...rest}
+                ref={ref}
             />
         </FormControl>
     )
 }
+
+export const Input = forwardRef(InputBase)
