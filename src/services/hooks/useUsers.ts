a12bd31108ba8d47718116dd8241a7b12d6e29af
NicholasNeto <nicholasneto@outlook.com>
2021-09-07 14:25:05 -0300

    fix type

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,4 +1,4 @@
-import { useQuery, UseQueryOptions } from "react-query"
+import { useQuery, UseQueryOptions, UseQueryResult } from "react-query"
 import { api } from "../api"
 
 
@@ -45,5 +45,5 @@ export function useUsers(page: number, options?: UseQueryOptions) {
     return useQuery(['users', page], () => getUsers(page), {
         staleTime: 1000 * 60 * 10, // 10 minutos não vai precisar se recarregado
         ...options
-    })
+    }) as UseQueryResult<GetUsersResponse, unknown>
 }
